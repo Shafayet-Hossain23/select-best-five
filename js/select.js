aa516e0ee389c81553feea18781963d39fa0021b
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-08-23 00:59:36 +0600

    extra-player-js

--- js/select.js
+++ js/select.js
@@ -17,3 +17,29 @@ document.getElementById('btn-mbappe').addEventListener('click', function () {
     playerSelection('mbappe', 'btn-mbappe')
 })
 
+
+document.getElementById('btn-benzema').addEventListener('click', function () {
+    playerSelection('benzema', 'btn-benzema')
+})
+document.getElementById('btn-kante').addEventListener('click', function () {
+    playerSelection('kante', 'btn-kante')
+})
+document.getElementById('btn-mane').addEventListener('click', function () {
+    playerSelection('mane', 'btn-mane')
+})
+document.getElementById('btn-ronaldo').addEventListener('click', function () {
+    playerSelection('ronaldo', 'btn-ronaldo')
+})
+document.getElementById('btn-lukaku').addEventListener('click', function () {
+    playerSelection('lukaku', 'btn-lukaku')
+})
+document.getElementById('btn-martinez').addEventListener('click', function () {
+    playerSelection('martinez', 'btn-martinez')
+})
+document.getElementById('btn-zlatan').addEventListener('click', function () {
+    playerSelection('zlatan', 'btn-zlatan')
+})
+document.getElementById('btn-de').addEventListener('click', function () {
+    playerSelection('de', 'btn-de')
+})
+
